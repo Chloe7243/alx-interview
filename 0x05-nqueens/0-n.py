2c0319f70ffd59c133deda82483115b1bbd74149
Stephanie <Stephaniechloe21@gmail.com>
2024-04-12 08:56:28 +0100

    file

--- 0x05-nqueens/0-n.py
+++ 0x05-nqueens/0-n.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ nQueens problem. """
-import sys
+from sys import argv, exit
 
 
 N = 0
@@ -47,16 +47,16 @@ def nQueens(board, col):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if len(argv) != 2:
         print("Usage: nqueens N")
-        sys.exit(1)
+        exit(1)
     try:
-        N = int(sys.argv[1])
+        N = int(argv[1])
         if N < 4:
             print("N must be at least 4")
-            sys.exit(1)
+            exit(1)
         board = [[0] * N for _ in range(N)]
         nQueens(board, 0)
     except ValueError:
         print("N must be a number")
-        sys.exit(1)
+        exit(1)
